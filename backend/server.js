dbb0f1f17fd8a01396416fe961f96cf61f817cb9
Edmund Lim <elimcw20@gmail.com>
2024-04-24 14:25:23 +0800

    Update server_files

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
-// connectDB = require('./src/db/db');
 const cors = require("cors");
 const helmet = require("helmet");
-
+const rateLimit = require("express-rate-limit");
 const auth = require("./src/routers/auth");
-
+const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -14,5 +13,18 @@ const limiter = rateLimit({
   legacyHeaders: false,
 });
 
-//   connectDB()
-const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/auth", auth);
+
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(5001, () => {
+  console.log(`Server started on port 5001`);
+});
