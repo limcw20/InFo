29a25594cb7f974a07f889d339b2b3262160b51e
Edmund Lim <elimcw20@gmail.com>
2024-04-24 14:25:58 +0800

    Create connection_test

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,4 +1,21 @@
-const { Pool } = require("pg");
+const pool = require("../db/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
+
+const getAllUsers = async (req, res) => {
+  try {
+    try {
+      const result = await pool.query("SELECT * FROM users");
+      res.json(result.rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "error getting users" });
+  }
+};
+
+module.exports = { getAllUsers };
