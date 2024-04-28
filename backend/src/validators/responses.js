d524ba74e97453b24f85877e1e8e0e8c10bb6885
Edmund Lim <elimcw20@gmail.com>
2024-04-29 01:17:42 +0800

    Update Typo

--- backend/src/validators/responses.js
+++ backend/src/validators/responses.js
@@ -1,9 +1,9 @@
 const { body } = require("express-validator");
 
 const validateResponseInPost = [
-  body("post_desc", "Description is required").not().isEmpty,
+  body("response_desc", "Description is required").not().isEmpty(),
   body(
-    "post_desc",
+    "response_desc",
     "Description must have a minimum of 1 and maximum of 1000 characters"
   ).isLength({
     min: 1,
