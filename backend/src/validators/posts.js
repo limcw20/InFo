dfacc5c573271a31e08c1a681c8123db94c0fda3
Edmund Lim <elimcw20@gmail.com>
2024-04-28 21:33:47 +0800

    Create post_validator

---
+++ backend/src/validators/posts.js
@@ -0,0 +1,23 @@
+const { body } = require("express-validator");
+
+const validateBodyInPost = [
+  body("post_title", "Title is required").not().isEmpty,
+  body(
+    "post_title",
+    "Title must have a minimum of 1 characters and 30 characters"
+  ).isLength({
+    min: 1,
+    max: 30,
+  }),
+
+  body("post_desc", "Description is required").not().isEmpty,
+  body(
+    "post_desc",
+    "Description must have a minimum of 1 and maximum of 1000 characters"
+  ).isLength({
+    min: 1,
+    max: 1000,
+  }),
+];
+
+module.exports = [validateBodyInPost];
