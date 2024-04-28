a434345bb247ebbc14b5a252145175f138d0d079
Edmund Lim <elimcw20@gmail.com>
2024-04-29 01:38:28 +0800

    Create Users_Validation

--- backend/src/routers/users.js
+++ backend/src/routers/users.js
@@ -7,12 +7,35 @@ const {
   deleteUserCategory,
 } = require("../controllers/users");
 const { authUser, authAdmin } = require("../middleware/auth");
+const {
+  validateUserSettings,
+  validateUpdateUserDetails,
+} = require("../validators/users");
+const { errorCheck } = require("../validators/errorCheck");
 
 const router = express.Router();
 
 router.post("/:username", authUser, getUserByUsername);
-router.patch("/:user_id", authUser, updateUserSettings);
-router.put("/:user_id/category", authUser, addUserCategory);
+router.patch(
+  "/:user_id",
+  validateUpdateUserDetails,
+  errorCheck,
+  authUser,
+  updateUserSettings
+);
+router.put(
+  "/:user_id/category",
+  authUser,
+  validateUserSettings,
+  errorCheck,
+  addUserCategory
+);
 router.get("/:user_id/category", authUser, getUserCategoryDetails);
-router.delete("/:user_settings_id/:user_id", authUser, deleteUserCategory);
+router.delete(
+  "/:user_settings_id/:user_id",
+  authUser,
+  validateUserSettings,
+  errorCheck,
+  deleteUserCategory
+);
 module.exports = router;
--- backend/src/validators/users.js
+++ backend/src/validators/users.js
@@ -1,5 +1,49 @@
 const { body } = require("express-validator");
 
-// const validateUserSettings = [];
+const validateUserSettings = [
+  body("category", "category is required").not().isEmpty(),
+  body(
+    "category",
+    "category must be a minimum of 3 and maximum of 24 characters"
+  ).isLength({
+    min: 3,
+    max: 24,
+  }),
+  body("sub_category", "category is required").not().isEmpty(),
+  body(
+    "sub_category",
+    "sub-category must be a minimum of 3 and maximum of 24 characters"
+  ).isLength({
+    min: 3,
+    max: 24,
+  }),
+];
 
-// const validateUpdateUserDetails = [];
+const validateUpdateUserDetails = [
+  body("nickname", "Nickname is required").not().isEmpty(),
+  body("nickname", "Nickname length min of 3 and max of 24").isLength({
+    min: 3,
+    max: 24,
+  }),
+  body("first_name", "First name is required").not().isEmpty(),
+  body("first_name", "First name max length is 50 characters").isLength({
+    max: 50,
+  }),
+  body(
+    "first_name",
+    "First name must contain only alphabetic characters"
+  ).matches(/^[a-zA-Z]+$/),
+
+  body("last_name", "Last name max length is 50 characters").isLength({
+    max: 50,
+  }),
+  body(
+    "last_name",
+    "Last name must contain only alphabetic characters"
+  ).matches(/^[a-zA-Z]+$/),
+  body("gender", "gender can only be male, female, or others")
+    .isString()
+    .matches(/^(male|female|others)$/),
+];
+
+module.exports = { validateUserSettings, validateUpdateUserDetails };
