cea71c69eb8d4c9b4685b26367b2f85ae6b3dcf7
Edmund Lim <elimcw20@gmail.com>
2024-04-29 17:55:22 +0800

    Create NavBar(WIP)

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.Navbar}>
+      <h1>InFo</h1>
+    </div>
+  );
+}; //input as ICEBOX
+
+export default Navbar;
