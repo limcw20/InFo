3e7455eb715658f3386e8d95f50126657abd4b7a
Edmund Lim <elimcw20@gmail.com>
2024-05-03 14:40:00 +0800

    Update ChatroomResponse.jsx

--- frontend/src/Components/ChatroomResponse.jsx
+++ frontend/src/Components/ChatroomResponse.jsx
@@ -68,7 +68,7 @@ const ChatroomResponse = (props) => {
 
   return (
     <>
-      <form onSubmit={handleSubmit} id="upload_image_widget">
+      <form onSubmit={handleSubmit}>
         <input
           className={styles.input}
           type="text"
@@ -88,7 +88,7 @@ const ChatroomResponse = (props) => {
         ></input>
         <button
           type="button"
-          form="upload_image_widget"
+          id="upload_image_widget"
           className={`${styles.cloudinary_button} cloudinary-button`}
         >
           Upload Picture
